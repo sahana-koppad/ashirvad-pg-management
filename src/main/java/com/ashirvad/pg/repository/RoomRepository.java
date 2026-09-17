ca4087e0e1df2699f9c541a2e1280a27ed175cdc
Sahana Koppad <sahanakoppad08@gmail.com>
2026-09-17 13:18:46 +0530

    Add Room Repository

---
+++ src/main/java/com/ashirvad/pg/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.ashirvad.pg.repository;
+
+
+import com.ashirvad.pg.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
