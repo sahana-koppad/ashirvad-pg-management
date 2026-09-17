bcd15eea5d73b78861e3b4a311c7bb25c5295a64
Sahana Koppad <sahanakoppad08@gmail.com>
2026-09-17 13:13:32 +0530

    Create Room Entity

---
+++ src/main/java/com/ashirvad/pg/entity/Room.java
@@ -0,0 +1,75 @@
+package com.ashirvad.pg.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roomNumber;
+    private String roomType;
+    private int totalBeds;
+    private int availableBeds;
+    private double monthlyRent;
+
+    public Room(Long id, String roomNumber, String roomType, int totalBeds, int availableBeds, double monthlyRent) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.totalBeds = totalBeds;
+        this.availableBeds = availableBeds;
+        this.monthlyRent = monthlyRent;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getTotalBeds() {
+        return totalBeds;
+    }
+
+    public void setTotalBeds(int totalBeds) {
+        this.totalBeds = totalBeds;
+    }
+
+    public int getAvailableBeds() {
+        return availableBeds;
+    }
+
+    public void setAvailableBeds(int availableBeds) {
+        this.availableBeds = availableBeds;
+    }
+
+    public double getMonthlyRent() {
+        return monthlyRent;
+    }
+
+    public void setMonthlyRent(double monthlyRent) {
+        this.monthlyRent = monthlyRent;
+    }
+}
+
