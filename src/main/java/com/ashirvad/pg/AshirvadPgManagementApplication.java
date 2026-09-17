e65db60c018096b7bd2b4b9789be025d51fccd70
Sahana Koppad <sahanakoppad08@gmail.com>
2026-09-17 12:57:15 +0530

    Create Spring Boot Backend and MySql configration

---
+++ src/main/java/com/ashirvad/pg/AshirvadPgManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ashirvad.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshirvadPgManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AshirvadPgManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashirvad/pg/AshirvadPgManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashirvad.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshirvadPgManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
