eb36cee2eee604c615dceab1c4512abe11ff9686
Sahana Koppad <sahanakoppad08@gmail.com>
2026-09-19 11:13:14 +0530

    Use HTTPS API endpoint

--- ashirvad-pg-frontend/src/services/api.js
+++ ashirvad-pg-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-13-61-146-142.eu-north-1.compute.amazonaws.com:8080/api",
+  baseURL: "https://boston-animation-propecia-life.trycloudflare.com/api",
   headers: {
     "Content-Type": "application/json",
   },
